3c5b680e6b32f8f6761477ea07ca384c44c0f207
steve mutuvi <stevemutuvi@gmail.com>
2020-09-29 08:52:41 +0300

    cased

--- bert-crf4NER/config.py
+++ bert-crf4NER/config.py
@@ -1,12 +1,12 @@
 class Config(object):	
 	apr_dir = '../model/'
 	data_dir = '../corpus/'
-	model_name = 'model_4.pt'
-	epoch = 5
+	model_name = 'model_2.pt'
+	epoch = 3
 	bert_model = 'bert-base-multilingual-cased'
 	lr = 5e-5
 	eps = 1e-8
-	batch_size = 8
+	batch_size = 16
 	mode = 'prediction' # for prediction mode = "prediction"
 	training_data = 'train.txt'
 	val_data = 'valid.txt'
